b641130fab4bc8e1330785ab54923639e3bf79cb
Gasper Vrhovsek <gasper.vrhovsek@gmail.com>
2017-09-26 12:25:05 +0200

    Add snap API call status check and update task configuration to newest snap version
    Create snap task only in case of non 400-500 response from API.
    Throw RequestException instead, which is handled down the line.
    Update configuration of snap task to work on snap version 2.0.0.
    Remove metrics fields not calculated by openfoam, snap throws
    error if fields are missing in openFOAM output. Rename web server
    fields and influxDB plugin name.
    
    Change-Id: I7c1a61515796642469628cb9def093e2607d51a1
    Signed-off-by: Gasper Vrhovsek <gasper.vrhovsek@gmail.com>

--- ofcloud/snap/api.py
+++ ofcloud/snap/api.py
@@ -9,6 +9,7 @@ def create_openfoam_task(target_ip):
             "type": "simple",
             "interval": "10s"
         },
+        "max-failures": 30,
         "workflow": {
             "collect": {
                 "metrics": {
@@ -18,54 +19,44 @@ def create_openfoam_task(target_ip):
                     "/intel/openfoam/Uz/initial": {},
                     "/intel/openfoam/p/final": {},
                     "/intel/openfoam/p/initial": {},
-                    "/intel/openfoam/k/final": {},
-                    "/intel/openfoam/k/initial": {},
-                    "/intel/openfoam/omega/final": {},
-                    "/intel/openfoam/omega/initial": {},
                     "/intel/openfoam/Uy/final": {},
                     "/intel/openfoam/Uy/initial": {}
                 },
                 "config": {
                     "/intel": {
-                        "swagIP": target_ip,
-                        "swagPort": 8000,
-                        "swagFile": "%2Frun.log?op=GET"
+                        "webServerIP": target_ip,
+                        "webServerPort": 8000,
+                        "webServerFilePath": "file/run.log?op=GET",
+                        "timeot": 20
                     }
                 },
-                "process": [
+                "process": None,
+                "publish": [
                     {
-                        "plugin_name": "passthru",
-                        "plugin_version": 1,
-                        "process": None,
-                        "publish": [
-                            {
-                                "plugin_name": "influx",
-                                "config": {
-                                    "host": settings.INFLUX_DB_HOST,
-                                    "port": settings.INFLUX_DB_PORT,
-                                    "database": settings.INFLUX_DB_NAME,
-                                    "user": settings.INFLUX_DB_USER,
-                                    "password": settings.INFLUX_DB_PASS
-                                }
+                        "plugin_name": "influxdb",
+                        "config": {
+                            "host": settings.INFLUX_DB_HOST,
+                            "port": settings.INFLUX_DB_PORT,
+                            "database": settings.INFLUX_DB_NAME,
+                            "user": settings.INFLUX_DB_USER,
+                            "password": settings.INFLUX_DB_PASS
+                        }
 
-                            }
-                        ],
-                        "config": None
                     }
                 ],
-                "publish": None
             }
         }
     }
 
     # Create the task
     r = requests.post(settings.SNAP_SERVICE + "tasks", json=task_manifest)
-    task_id = r.json()["body"]["id"]
-
-    # Start the task
-    r = requests.put(settings.SNAP_SERVICE + "tasks/%s/start" % task_id)
-
-    return task_id
+    if r.status_code >= 400:
+        raise requests.RequestException("Error accessing Snap API, status_code = %s" % r.status_code, r.content)
+    else:
+        task_id = r.json()["body"]["id"]
+        # Start the task
+        r = requests.put(settings.SNAP_SERVICE + "tasks/%s/start" % task_id)
+        return task_id
 
 
 def stop_openfoam_task(task_id):
