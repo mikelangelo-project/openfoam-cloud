427bde90dcbcca90674906f30879c03235fa518b
Gasper Vrhovsek <gasper.vrhovsek@xlab.si>
2017-10-05 15:10:18 +0200

    Update license information
    Change-Id: Ieea07b781c33ce64a6669b6dfb7a740fcdc1b469
    Signed-off-by: Gasper Vrhovsek <gasper.vrhovsek@xlab.si>

--- setup.py
+++ setup.py
@@ -1,3 +1,18 @@
+# Copyright (C) 2015-2017 XLAB, Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 import os
 from setuptools import find_packages, setup
 
@@ -14,7 +29,7 @@ setup(
     version='1.0',
     packages=find_packages(exclude=['']),
     include_package_data=True,
-    license='BSD License',
+    license='Apache Software License',
     description='OpenFOAM Cloud backend application',
     long_description=README,
     url='https://www.xlab.si/',
@@ -23,7 +38,7 @@ setup(
     classifiers=[
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
